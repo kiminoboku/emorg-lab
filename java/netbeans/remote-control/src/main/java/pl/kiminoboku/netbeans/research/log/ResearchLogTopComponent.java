e6be64dc7f640e14bade38e73d09873a0ad6d2b1
Radoslaw Lewandowski <kiminoboku@gmail.com>
2015-03-16 11:53:51 +0100

    Added exporting of research logs to csv files

--- java/netbeans/remote-control/src/main/java/pl/kiminoboku/netbeans/research/log/ResearchLogTopComponent.java
+++ java/netbeans/remote-control/src/main/java/pl/kiminoboku/netbeans/research/log/ResearchLogTopComponent.java
@@ -7,9 +7,20 @@ package pl.kiminoboku.netbeans.research.log;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import java.awt.event.ActionEvent;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import org.jdesktop.swingx.treetable.AbstractTreeTableModel;
 import org.netbeans.api.settings.ConvertAsProperties;
 import org.openide.awt.ActionID;
@@ -47,6 +58,8 @@ public final class ResearchLogTopComponent extends TopComponent {
      * Component action category
      */
     public static final String ACTION_CATEGORY = "Window";
+    
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 
     /**
      * Component action id
@@ -153,8 +166,16 @@ public final class ResearchLogTopComponent extends TopComponent {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jXTreeTable1 = new org.jdesktop.swingx.JXTreeTable();
+        jButton1 = new javax.swing.JButton();
 
         jScrollPane1.setViewportView(jXTreeTable1);
+
+        org.openide.awt.Mnemonics.setLocalizedText(jButton1, org.openide.util.NbBundle.getMessage(ResearchLogTopComponent.class, "ResearchLogTopComponent.jButton1.text")); // NOI18N
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -162,18 +183,44 @@ public final class ResearchLogTopComponent extends TopComponent {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 297, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton1)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 275, Short.MAX_VALUE)
-                .addGap(14, 14, 14))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton1)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 275, Short.MAX_VALUE)
+                        .addGap(14, 14, 14))))
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        JFileChooser jFileChooser = new JFileChooser();
+        jFileChooser.setAcceptAllFileFilterUsed(false);
+        jFileChooser.setApproveButtonText("Save");
+        jFileChooser.setDialogTitle("Save as csv");
+        jFileChooser.setDialogType(JFileChooser.SAVE_DIALOG);
+        jFileChooser.setFileFilter(new FileNameExtensionFilter("CSV File", "csv"));
+        jFileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        jFileChooser.setMultiSelectionEnabled(false);
+        int result = jFileChooser.showSaveDialog(null);
+        if(result == JFileChooser.APPROVE_OPTION) {
+            File savedCsv = jFileChooser.getSelectedFile();
+            exportData(savedCsv);
+        }
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JScrollPane jScrollPane1;
     private org.jdesktop.swingx.JXTreeTable jXTreeTable1;
     // End of variables declaration//GEN-END:variables
@@ -198,6 +245,37 @@ public final class ResearchLogTopComponent extends TopComponent {
     void readProperties(java.util.Properties p) {
         String version = p.getProperty("version");
         // TODO read your settings according to their version
+    }
+
+    private void exportData(File savedCsv) {
+        try {
+            try(BufferedWriter writer = new BufferedWriter(new FileWriter(savedCsv))) {
+                writer.write("Event;Time;Description\n");
+                for(ResearchLog researchLog : logs) {
+                    String event = Column.EVENT.getValue(researchLog).toString();
+                    String time = DATE_FORMAT.format(researchLog.getResearchStartTime());
+                    String description = Column.DESCRIPTION.getValue(researchLog).toString();
+                    writeDataLine(writer, event, time, description);
+                    
+                    for(OperationLog operationLog : researchLog.getOperationLogs()) {
+                        event = Column.EVENT.getValue(operationLog).toString();
+                        time = DATE_FORMAT.format(operationLog.getOperationTime());
+                        description = operationLog.getLogMessage();
+                        writeDataLine(writer, event, time, description);
+                    }
+                }
+                
+                JOptionPane.showMessageDialog(null, "File saved");
+            }
+        } catch(IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    
+    
+    private void writeDataLine(BufferedWriter writer, String event, String time, String description) throws IOException {
+        String message = MessageFormat.format("{0};{1};{2}\n", event, time, description);
+        writer.write(message);
     }
 
     public static enum Column {
